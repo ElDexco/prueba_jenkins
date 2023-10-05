ddd99d726cb0a78a2b1a006bac422622a0324ba3
Lucas Dextre <lucasdexco@gmail.com>
2023-10-04 21:50:45 -0300

    prueba para jenkins primer commit

---
+++ src/main/java/com/mycompany/prueba_jenkins/Prueba_jenkins.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.prueba_jenkins;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Prueba_jenkins {
+
+    public static void main(String[] args) {
+        System.out.println("A commit has been made.");
+    }
+}
